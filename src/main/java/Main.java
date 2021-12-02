ea648b3f71aef6e06cf48af730eef7949f78b737
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-12-02 16:26:42 -0300

    Solved challenge 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Challenge1 challenge1 = new Challenge1();
-        challenge1.part1(true);
+        challenge1.part2(false);
 
     }
 }
---
+++ src/main/java/challenges/challenge1/Challenge1.java
@@ -0,0 +1,68 @@
+package challenges.challenge1;
+
+import models.FileHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Challenge1 {
+    String pathexample = "src/main/java/challenges/challenge1/example.txt";
+    String pathchallenge = "src/main/java/challenges/challenge1/challenge.txt";
+    FileHandler fileHandler;
+
+    public Challenge1() {
+    }
+
+    private void setFilePath(Boolean isTestingExample){
+        if (isTestingExample){
+            this.fileHandler = new FileHandler(pathexample);
+        } else {
+            this.fileHandler = new FileHandler(pathchallenge);
+        }
+    }
+
+    public void part1(Boolean isTestingExample){
+        setFilePath(isTestingExample);
+        List<String> thedata = this.fileHandler.readFile();
+        List<Integer> totest = new ArrayList<>();
+        for (String item: thedata) {
+            totest.add(Integer.parseInt(item));
+        }
+        for (Integer index1: totest) {
+            for (Integer index2: totest) {
+                if (index1+index2 == 2020){
+                    System.out.println("Items found.");
+                    System.out.println(index1 + " + " + index2 + "= 2020");
+                    System.out.println(index1 + " * " + index2 + "= " + (index1*index2));
+                    break;
+                }
+            }
+        }
+    }
+
+    public void part2(Boolean isTestingExample){
+        setFilePath(isTestingExample);
+        List<String> thedata = this.fileHandler.readFile();
+        List<Integer> totest = new ArrayList<>();
+        for (String item: thedata) {
+            totest.add(Integer.parseInt(item));
+        }
+        for (Integer index1: totest) {
+            for (Integer index2: totest) {
+                for (Integer index3: totest) {
+                    if (index1+index2+index3 == 2020){
+                        System.out.println("Items found.");
+                        System.out.println(index1 + " + " + index2 + " + " + index3 + "= 2020");
+                        System.out.println(index1 + " * " + index2 + " * " + index3 + "= " + (index1*index2*index3));
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+
+}
